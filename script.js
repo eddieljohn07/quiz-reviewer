cf7ed6424cd06660ec4c3214822782f65ba8332e
eddieljohn07 <100287870+eddieljohn07@users.noreply.github.com>
2023-04-23 16:20:47 +0800

    initialized variables

---
+++ script.js
@@ -0,0 +1,6 @@
+const questionInput = document.querySelector('#question');
+const answerInput = document.querySelector('#answer');
+const addBtn = document.querySelector('#add-btn');
+const startBtn = document.querySelector('#start-btn');
+const reshuffleBtn = document.querySelector('#reshuffle-btn');
+const questionsSection = document.querySelector('#questions-section');
