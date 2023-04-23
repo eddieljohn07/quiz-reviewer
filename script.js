e443e72e397c51318ec31f2fa9f6b2e0ca91043e
eddieljohn07 <100287870+eddieljohn07@users.noreply.github.com>
2023-04-23 17:34:10 +0800

    add function and event listener to display input

--- script.js
+++ script.js
@@ -24,7 +24,36 @@ startBtn.addEventListener('click', () => {
 });
 function displayQuestions() {
     questionsSection.innerHTML = '';
-    quizData.forEach((data, index)) => {
-        
-    }
+    quizData.forEach((data, index) => {
+      const questionCard = document.createElement('div');
+      questionCard.classList.add('question-card');
+      const questionTitle = document.createElement('h2');
+      questionTitle.textContent = `${index + 1}. ${data.question}`;
+      const answerText = document.createElement('p');
+      answerText.classList.add('answer');
+      answerText.textContent = `Answer: ${data.answer}`;
+      const buttonsDiv = document.createElement('div');
+      buttonsDiv.classList.add('buttons');
+      const revealBtn = document.createElement('button');
+      revealBtn.textContent = 'Reveal';
+      const hideBtn = document.createElement('button');
+      hideBtn.textContent = 'Hide';
+      hideBtn.style.display = 'none';
+      revealBtn.addEventListener('click', () => {
+        answerText.style.display = 'block';
+        revealBtn.style.display = 'none';
+        hideBtn.style.display = 'inline-block';
+      });
+      hideBtn.addEventListener('click', () => {
+        answerText.style.display = 'none';
+        hideBtn.style.display = 'none';
+        revealBtn.style.display = 'inline-block';
+      });
+      buttonsDiv.appendChild(revealBtn);
+      buttonsDiv.appendChild(hideBtn);
+      questionCard.appendChild(questionTitle);
+      questionCard.appendChild(answerText);
+      questionCard.appendChild(buttonsDiv);
+      questionsSection.appendChild(questionCard);
+    });
 }
