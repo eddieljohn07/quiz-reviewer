5738c70c466a8dfa34d7d4f8b8a5530c3f0ec64c
eddieljohn07 <100287870+eddieljohn07@users.noreply.github.com>
2023-04-23 16:29:07 +0800

    add array

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ const addBtn = document.querySelector('#add-btn');
 const startBtn = document.querySelector('#start-btn');
 const reshuffleBtn = document.querySelector('#reshuffle-btn');
 const questionsSection = document.querySelector('#questions-section');
+let quizData = [];
